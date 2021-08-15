c0f1ef1fc2dc50f25d5813a8167b92e515e04f5d
miki hayden <miki.palao.palmer@gmail.com>
2021-08-15 21:12:15 +0200

    Last changes added

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 
-import POLLITO from '../../assets/img/pollo.svg';
 import './index.scss';
 
 const Navbar = () => (
   <nav className="navbar">
     <div className="navbar__container">
-      <img src={POLLITO} alt="pollito" />
-      <p className="navbar__title">Pelis de princesas para la princesa</p>
+      {/* <img src={LOGO} alt="logo" /> */}
+      <p className="navbar__title">BRAND Name</p>
     </div>
   </nav>
   );
