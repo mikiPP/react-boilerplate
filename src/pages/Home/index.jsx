7d5b22ca7444e4d562d81ac1ccbebd021e049222
miki hayden <miki.palao.palmer@gmail.com>
2021-10-04 01:07:17 +0200

    feat(Home): Home page refactored

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,36 +1,24 @@
 /* eslint-disable no-console */
-import React, { useEffect } from 'react';
+import React, { useContext } from 'react';
 import Slider from '../../components/Slider/Slider';
-import Spinner from '../../components/Spinner';
+import AuthContext from '../../contexts/authContext';
 
 import './index.scss';
 
 const images = [ 'https://images.pexels.com/photos/4164418/pexels-photo-4164418.jpeg?cs=srgb&dl=pexels-antonio-batini%C4%87-4164418.jpg&fm=jpg', 'https://images.pexels.com/photos/325111/pexels-photo-325111.jpeg?cs=srgb&dl=pexels-marc-mueller-325111.jpg&fm=jpg', 'https://images.pexels.com/photos/52608/pexels-photo-52608.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260', 'https://images.pexels.com/photos/574073/pexels-photo-574073.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260' ];
 
 const HomePage = () => {
-  useEffect(() => {
-    console.log('Voy a ejecutarme una vez');
-
-    console.log('Primero');
-  }, []);
-
-  useEffect(() => {
-    console.log('Voy a ejecutarme a cada render');
-
-    console.log('segundo');
-  });
-
-  useEffect(() => {
-
-  }, []);
+  const auth = useContext(AuthContext);
 
   return (
     <div id="home-page">
+      <header className="header" >
+        <h1> Hi {auth.email || 'Guest'} ! </h1>
+      </header>
       <section className="slider-wrapper">
         <Slider images={images} />
       </section>
     </div>
   );
 };
-
 export default HomePage;
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import { Route, Switch } from 'react-router';
 import Navbar from '../components/Navbar';
-import { HOME_URL } from '../utils/urls';
+import { HOME_URL, LOGIN_URL } from '../utils/urls';
 import '../styles/base/_base.scss';
 import HomePage from './Home';
+import LoginPage from './Login';
 
 const Main = () => (
   <main className="app">
     <Navbar />
-    {/* TODO: AÑADIR RUTAS */}
+    {/* TODO: AÑADIR RUTA DEFAULT */}
     <Switch>
       <Route exact path={HOME_URL} component={HomePage} />
+      <Route exact path={LOGIN_URL} component={LoginPage} />
     </Switch>
   </main>
 );
