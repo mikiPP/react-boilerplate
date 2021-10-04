a591376d2d00af052699185c2402a1668923fcd1
miki hayden <miki.palao.palmer@gmail.com>
2021-10-04 22:38:19 +0200

    refactor(Api): Changed api to a dummy one.

--- src/api/todo/index.js
+++ src/api/todo/index.js
@@ -1,43 +1,16 @@
 import axios from 'axios';
-import { generateUUID } from '../../utils/functions';
 
-const BASE_PATH = 'url';
+const BASE_PATH = 'https://jsonplaceholder.typicode.com';
 
-export const getTodos = (queryParams) =>
+// eslint-disable-next-line import/prefer-default-export
+export const getPosts = () =>
   new Promise((resolve, reject) => {
     axios
-      .get(`${BASE_PATH}/todos${queryParams}`)
+      .get(`${BASE_PATH}/photos`)
       .then((response) => {
-          resolve(response.data.body);
+          resolve(response.data);
       })
       .catch((err) => {
         reject(err);
       });
-  });
-
-export const createTodo = (todo) =>
-  new Promise((resolve, reject) => {
-    const _id = generateUUID();
-    const todoFormatted = { ...todo, watched: todo.watched || false, _id };
-
-    axios
-    .post(`${BASE_PATH}/todo`, todoFormatted)
-    .then((response) => {
-      resolve(response.data.body);
-    })
-    .catch((err) => {
-      reject(err);
-    });
-  });
-
-export const updateTodo = (todo) =>
-  new Promise((resolve, reject) => {
-    axios
-    .put(`${BASE_PATH}/todo`, todo)
-    .then((response) => {
-      resolve(response.data.body);
-    })
-    .catch((err) => {
-      reject(err);
-    });
   });
