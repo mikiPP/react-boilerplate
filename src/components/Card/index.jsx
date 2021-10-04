cffa937fe62334934a64e4535a7bbb61e96de6c2
miki hayden <miki.palao.palmer@gmail.com>
2021-10-04 22:40:49 +0200

    feat(Home page): Dummy call added to the home page.

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -4,10 +4,12 @@ import PropTypes from 'prop-types';
 import './index.scss';
 import Button from '../Button';
 
+const POINT_FIVE = 0.5;
+
 const Card = ({ todo, onUpdate }) => (
   <div className="card">
     <div className="card__content">
-      <div style={todo.imgUrl ? { backgroundImage: `url(${todo.imgUrl})` } : null} className={`card__img ${todo?.watched ? 'img-black-withe' : ''}`} />
+      <div style={todo.url ? { backgroundImage: `url(${todo.url})` } : null} className={`card__img ${Math.random() > POINT_FIVE ? 'img-black-withe' : ''}`} />
       <div className="card__title" >
         <span>{todo?.title}</span>
         <Button text="Editar" className="card__button" onClick={() => onUpdate(todo)} />
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,19 +1,47 @@
 /* eslint-disable no-console */
-import React, { useContext } from 'react';
-import Slider from '../../components/Slider/Slider';
+import React, { useContext, useEffect, useState } from 'react';
+import { getPosts } from '../../api/todo';
+import Slider from '../../components/Slider';
+import Spinner from '../../components/Spinner';
+import Card from '../../components/Card';
 import AuthContext from '../../contexts/authContext';
+import { isEmpty } from '../../utils/functions';
 
 import './index.scss';
+import { ZERO } from '../../utils/constants';
 
 const images = [ 'https://images.pexels.com/photos/4164418/pexels-photo-4164418.jpeg?cs=srgb&dl=pexels-antonio-batini%C4%87-4164418.jpg&fm=jpg', 'https://images.pexels.com/photos/325111/pexels-photo-325111.jpeg?cs=srgb&dl=pexels-marc-mueller-325111.jpg&fm=jpg', 'https://images.pexels.com/photos/52608/pexels-photo-52608.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260', 'https://images.pexels.com/photos/574073/pexels-photo-574073.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260' ];
 
 const HomePage = () => {
   const auth = useContext(AuthContext);
 
+  const [ loading, setLoading ] = useState(false);
+  const [ posts, setPosts ] = useState([]);
+
+  const CARDS_TO_SHOW = 12;
+
+  useEffect(() => {
+    setLoading(true);
+
+    getPosts()
+      .then((dbPosts) => {
+        const dbPostsCopy = [ ...dbPosts ].splice(ZERO, CARDS_TO_SHOW);
+        setPosts(dbPostsCopy);
+      })
+      .catch((err) => console.error(err))
+      .finally(() => setLoading(false));
+  }, []);
+
   return (
     <div id="home-page">
       <header className="header" >
-        <h1> Hi {auth.email || 'Guest'} ! </h1>
+        <h1 className="header__title"> Hi {auth.email || 'Guest'} ! </h1>
+
+        {loading && <Spinner />}
+
+        <div className="header__cards">
+          {!isEmpty(posts) && posts.map((post) => <Card todo={post} />)}
+        </div>
       </header>
       <section className="slider-wrapper">
         <Slider images={images} />
